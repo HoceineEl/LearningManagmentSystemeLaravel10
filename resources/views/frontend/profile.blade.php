9387c6e0b3c0814c3dab2438720159b29c188e0e
Abdallah <abdallahradfi@gmail.com>
2023-06-10 17:21:14 +0100

    fixed some bugs in the update profile functionnality, and added the cancel button

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -106,95 +106,125 @@
         @endif
     </div>
 </div> --}}
-        @if (session('success'))
-            <div class="container">
-                <div class="alert alert-success alert-dismissible fade show">
-                    <div class="d-flex align-items-center">
-                        <div><strong>{{ session('success') }}</strong></div>
-                        <div class="mt-0">
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
+    @if (session('success'))
+        <div class="container">
+            <div class="alert alert-success alert-dismissible fade show">
+                <div class="d-flex align-items-center">
+                    <div><strong>{{ session('success') }}</strong></div>
+                    <div class="mt-0">
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 </div>
             </div>
-        @endif
-        <div class="container mt-4">
-            <div class="card">
-                <div class="card-body">
-                    <h2 class="card-title">Profile</h2>
-                    <form id="profileForm" method="POST" action="{{ route('profile.update') }}">
-                        @csrf
-                        <input type="hidden" name="user_id" value="{{ $user->id }}">
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Name:</label>
-                            <input type="text" class="form-control" id="name" name="name"
-                                value="{{ $user->name }}" readonly>
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email:</label>
-                            <input type="email" class="form-control" id="email" name="email"
-                                value="{{ $user->email }}" readonly>
-                        </div>
+        </div>
+    @endif
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">Profile</h2>
+                <form id="profileForm" method="POST" action="{{ route('profile.update') }}">
+                    @csrf
+                    <input type="hidden" name="user_id" value="{{ $user->id }}">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name:</label>
+                        <input type="text" class="form-control" id="name" name="name" value="{{ $user->name }}"
+                            readonly>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}"
+                            readonly>
+                    </div>
 
-                        <button class="btn btn-success mt-2" id="modifyButton" type="button">
-                            Modify
-                        </button>
-                        <button class="btn btn-primary mt-2" id="saveButton" type="submit" style="display: none;">
-                            Save
-                        </button>
-                    </form>
-                </div>
+                    <button class="btn btn-success mt-2" id="modifyButton" type="button">
+                        Modify
+                    </button>
+                    <button class="btn btn-primary mt-2" id="saveButton" type="submit" style="display: none;">
+                        Save
+                    </button>
+                    <button class="btn btn-secondary mt-2" id="cancelButton" type="button" style="display: none;">
+                        Cancel
+                    </button>
+                </form>
             </div>
+        </div>
 
-            <div class="card">
-                <div class="card-body">
-                    <h2 class="card-title">Courses Progress</h2>
-                    @forelse ($coursesprogression as $progression)
-                        <div class="card m-0 courseBox">
-                            <div class="d-flex justify-content-between">
-                                <div class="m-3">
-                                    <p class="fw-bold mb-0">{{ $progression->cour_id }}</p>
-                                    @if ($progression->est_complete)
-                                        <small class="mt-0">Completed</small>
-                                    @else
-                                        <small class="mt-0">In Progress</small>
-                                    @endif
-                                </div>
-                                <div class="align-self-center me-3">
-                                    <div class="btn-group dropleft">
-                                        <a class="text-decoration-none" id="dropdownMenuButton" data-toggle="dropdown"
-                                            aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v"></i>
-                                        </a>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <a class="dropdown-item" href="#">Visit Course</a>
-                                            <a class="dropdown-item" href="#">Delete Course</a>
-                                        </div>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">Courses Progress</h2>
+                @forelse ($coursesprogression as $progression)
+                    <div class="card m-0 courseBox">
+                        <div class="d-flex justify-content-between">
+                            <div class="m-3">
+                                <p class="fw-bold mb-0">{{ $progression->cour_id }}</p>
+                                @if ($progression->est_complete)
+                                    <small class="mt-0">Completed</small>
+                                @else
+                                    <small class="mt-0">In Progress</small>
+                                @endif
+                            </div>
+                            <div class="align-self-center me-3">
+                                <div class="btn-group dropleft">
+                                    <a class="text-decoration-none" id="dropdownMenuButton" data-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="false">
+                                        <i class="fas fa-ellipsis-v"></i>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">Visit Course</a>
+                                        <a class="dropdown-item" href="#">Delete Course</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    @empty
-                        <p class="fw-bold my-2">You did not enroll any course yet</p>
-                    @endforelse
-                </div>
+                    </div>
+                @empty
+                    <p class="fw-bold my-2">You did not enroll any course yet</p>
+                @endforelse
             </div>
         </div>
+    </div>
     <script>
         let modifyButton = document.getElementById('modifyButton');
         let saveButton = document.getElementById('saveButton');
+        let cancelButton = document.getElementById('cancelButton');
         let form = document.getElementById('profileForm');
+        let inputs = document.getElementsByTagName("input");
+
+        // Store the original values in a data attribute
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].setAttribute('data-original-value', inputs[i].value);
+        }
 
         modifyButton.addEventListener('click', () => {
-            let inputs = document.getElementsByTagName("input");
             for (let i = 0; i < inputs.length; i++) {
                 let input = inputs[i];
-                // Toggle the readonly property
                 input.readOnly = !input.readOnly;
+                input.classList.toggle('editable-input');
+
+                if (!input.readOnly) {
+                    input.focus();
+                    input.classList.add('focused-input');
+                } else {
+                    input.classList.remove('focused-input');
+                }
             }
 
             modifyButton.style.display = 'none';
-            saveButton.style.display = 'block';
+            saveButton.style.display = 'inline';
+            cancelButton.style.display = 'inline';
+        });
+
+        cancelButton.addEventListener('click', () => {
+            for (let i = 0; i < inputs.length; i++) {
+                let input = inputs[i];
+                input.readOnly = true;
+                input.classList.remove('editable-input', 'focused-input');
+                input.value = input.getAttribute('data-original-value'); // Restore the original value
+            }
+
+            modifyButton.style.display = 'block';
+            saveButton.style.display = 'none';
+            cancelButton.style.display = 'none';
         });
 
         form.addEventListener('submit', (event) => {
